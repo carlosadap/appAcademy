8b9c5157b1c1cc8dea452276316b5035144debf4
Carlos Pinheiro <carlosadap@gmail.com>
2021-01-25 13:20:52 +0100

    part 2

--- projects/battleship_files/battleship_project/lib/board.rb
+++ projects/battleship_files/battleship_project/lib/board.rb
@@ -2,23 +2,80 @@ class Board
   attr_reader :size
 
   def initialize(n)
-    @grid = Array.new(n) { |i| i = Array.new(n, :N) }
+    @grid = Array.new(n) { Array.new(n, :N) }
     @size = n*n
   end
 
   def [](arr)
-    @grid[arr[0]][arr[1]]
+    # @grid[arr[0]][arr[1]]
+    row, col = arr
+    @grid[row][col]
   end
 
   def []=(position, value)
-    @grid[position[0]][position[1]] = value
+    row, col = position
+    @grid[row][col] = value
   end
 
   def num_ships
-    sum = 0
+    # sum = 0
 
-    @grid.each { |row| sum += row.count(:S) }
+    # @grid.each { |row| sum += row.count(:S) }
+    # sum
 
-    sum
+    @grid.flatten.count { |ele| ele == :S }
+  end
+
+  def attack(position)
+    # row, col = position
+
+    if self[position] == :S
+      self[position] = :H
+      puts "you sunk my battleship!"
+      return true
+    else
+      self[position] = :X
+      return false
+    end
+  end
+
+  def place_random_ships
+    rand_position = []
+    n = @grid.length
+
+    while rand_position.length < @size/4
+      num = [rand(0...n),rand(0...n)]
+      rand_position << num if !rand_position.include?(num)
+    end
+    
+    rand_position.each { |pos| self[pos] = :S }
+  end
+
+  def hidden_ships_grid
+    new_arr = Array.new(@grid.length) { Array.new(@grid.length) }
+    
+    @grid.each_with_index do |subarray, i| 
+      subarray.each_with_index do |ele, j|
+        if ele == :S
+          new_arr[i][j] = :N
+        else
+          new_arr[i][j] = ele
+        end
+      end
+    end
+
+    new_arr
+  end
+
+  def self.print_grid(array)
+    array.each { |subarray| puts "#{subarray.join(" ")}\n" }
+  end
+
+  def cheat
+    Board.print_grid(@grid)
+  end
+
+  def print
+    Board.print_grid(self.hidden_ships_grid)
   end
 end
