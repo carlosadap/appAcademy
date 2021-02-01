163e9af665e1b1c988e1594a9439ab81ec3c4e6d
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-01 10:21:07 +0100

    done matrix_addition_reloeaded

--- exercises/nauseating_numbers/nauseating_numbers.rb
+++ exercises/nauseating_numbers/nauseating_numbers.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 def strange_sums(arr)
   count = 0
   
@@ -15,6 +17,7 @@ p strange_sums([42, 3, -1, -42])      # 1
 p strange_sums([-5, 5])               # 1
 p strange_sums([19, 6, -3, -20])      # 0
 p strange_sums([9])                   # 0
+puts "----------"
 
 def pair_product(arr, product)
 
@@ -34,6 +37,7 @@ p pair_product([3, 4, 6, 2, 5], 12) # true
 p pair_product([4, 2, 5, 7], 16)    # false
 p pair_product([8, 4, 9, 3], 8)     # false
 p pair_product([3], 12)             # false
+puts "----------"
 
 def rampant_repeats(str, hash)
   chars = str.split("")
@@ -54,6 +58,7 @@ p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
 p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
 p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
 p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
+puts "----------"
 
 def perfect_square(num)
   factor = 1
@@ -77,3 +82,134 @@ p perfect_square(2)     # false
 p perfect_square(40)    # false
 p perfect_square(32)    # false
 p perfect_square(50)    # false
+puts "----------"
+
+def anti_prime?(num)
+  hash = { }
+
+  (1..num).each { |n| hash[n] = n_divisors(n) }
+
+  hash.max_by{|k,v| v} [0] == num
+
+end
+
+def n_divisors(num)
+  count = 0
+
+  (1..num).each { |n| count += 1 if num % n == 0 }
+
+  count
+end
+
+p anti_prime?(24)   # true
+p anti_prime?(36)   # true
+p anti_prime?(48)   # true
+p anti_prime?(360)  # true
+p anti_prime?(1260) # true
+p anti_prime?(27)   # false
+p anti_prime?(5)    # false
+p anti_prime?(100)  # false
+p anti_prime?(136)  # false
+p anti_prime?(1024) # false
+puts "----------"
+
+def matrix_addition(arr1, arr2)
+  row_length = arr1.length
+  col_length = arr1[0].length
+  sum_arr = Array.new(row_length) {Array.new(col_length)}
+
+  (0...row_length).each do |row|
+    (0...col_length).each do |col|
+      # debugger
+      sum_arr[row][col] = arr1[row][col] + arr2[row][col]
+    end
+  end
+
+  sum_arr
+end
+
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+
+p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+puts "----------"
+
+def mutual_factors(*nums)
+  factors_arr = []
+
+  (1..nums.min).each do |factor|
+    factors_arr << factor if nums.all? { |num| num % factor == 0 }
+  end
+
+  factors_arr
+end
+  p mutual_factors(50, 30)            # [1, 2, 5, 10]
+  p mutual_factors(50, 30, 45, 105)   # [1, 5]
+  p mutual_factors(8, 4)              # [1, 2, 4]
+  p mutual_factors(8, 4, 10)          # [1, 2]
+  p mutual_factors(12, 24)            # [1, 2, 3, 4, 6, 12]
+  p mutual_factors(12, 24, 64)        # [1, 2, 4]
+  p mutual_factors(22, 44)            # [1, 2, 11, 22]
+  p mutual_factors(22, 44, 11)        # [1, 11]
+  p mutual_factors(7)                 # [1, 7]
+  p mutual_factors(7, 9)              # [1]
+  puts "----------"
+
+def tribonacci_number(num)
+  fib_arr = [1, 1, 2]
+  
+  if num < 4
+    return fib_arr[num - 1]
+  end
+
+  (4..num).each do |n|
+    fib_arr << fib_arr[n - 2] + fib_arr[n - 3] + fib_arr[n - 4]
+  end
+
+  fib_arr[num - 1]
+end
+
+p tribonacci_number(1)  # 1
+p tribonacci_number(2)  # 1
+p tribonacci_number(3)  # 2
+p tribonacci_number(4)  # 4
+p tribonacci_number(5)  # 7
+p tribonacci_number(6)  # 13
+p tribonacci_number(7)  # 24
+p tribonacci_number(11) # 274
+puts "----------"
+
+def matrix_addition_reloaded(*arrs)  
+  return nil if arrs.any? { |arr| arr.length != arrs[0].length }
+    
+  arrs.each do |matrix|
+    # debugger
+    return nil if matrix.any? { |col| col.length != matrix[0].length }
+  end
+
+  arrs.inject do |accu, matrix|
+      matrix_addition(accu, matrix)
+  end
+end
+
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+matrix_f = [[0 , 0], [12, 4, 4], [6,  3]]
+
+p matrix_addition_reloaded(matrix_a, matrix_b)              # [[11, 6], [7, 7]]
+p matrix_addition_reloaded(matrix_a, matrix_b, matrix_c)    # [[10, 6], [7, 6]]
+p matrix_addition_reloaded(matrix_e)                        # [[0, 0], [12, 4], [6, 3]]
+p matrix_addition_reloaded(matrix_d, matrix_e)              # [[2, -5], [19, 14], [6, 4]]
+p matrix_addition_reloaded(matrix_a, matrix_b, matrix_e)    # nil
+p matrix_addition_reloaded(matrix_d, matrix_e, matrix_c)    # nil
+p matrix_addition_reloaded(matrix_d, matrix_e, matrix_f)    # nil
+puts "----------"
--- exercises/nauseating_numbers/readme.md
+++ exercises/nauseating_numbers/readme.md
@@ -66,3 +66,117 @@ p perfect_square(40)    # false
 p perfect_square(32)    # false
 p perfect_square(50)    # false
 ```
+
+## Phase 2: Nothing you can't handle.
+
+### anti_prime?
+
+Write a method `anti_prime?` that accepts a number as an argument. The method should return true if the given number has more divisors than all positive numbers less than the given number. For example, 24 is an anti-prime because it has more divisors than any positive number less than 24. Math Fact: Numbers that meet this criteria are also known as [highly composite numbers.](https://en.wikipedia.org/wiki/Highly_composite_number)
+
+Examples
+
+```ruby
+p anti_prime?(24)   # true
+p anti_prime?(36)   # true
+p anti_prime?(48)   # true
+p anti_prime?(360)  # true
+p anti_prime?(1260) # true
+p anti_prime?(27)   # false
+p anti_prime?(5)    # false
+p anti_prime?(100)  # false
+p anti_prime?(136)  # false
+p anti_prime?(1024) # false
+```
+
+### matrix_addition
+
+Let a 2-dimensional array be known as a "matrix". Write a method `matrix_addition` that accepts two matrices as arguments. The two matrices are guaranteed to have the same "height" and "width". The method should return a new matrix representing the sum of the two arguments. To add matrices, we simply add the values at the same positions:
+
+#### programmatically
+[[2, 5], [4, 7]] + [[9, 1], [3, 0]] => [[11, 6], [7, 7]]
+
+#### structurally
+2 5  +  9 1  =>  11 6
+4 7     3 0      7 7
+
+Examples
+
+```ruby
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+
+p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+```
+
+### mutual_factors
+
+Write a method `mutual_factors` that accepts any amount of numbers as arguments. The method should return an array containing all of the common divisors shared among the arguments. For example, the common divisors of 50 and 30 are 1, 2, 5, 10. You can assume that all of the arguments are positive integers.
+
+Examples
+
+
+```ruby
+p mutual_factors(50, 30)            # [1, 2, 5, 10]
+p mutual_factors(50, 30, 45, 105)   # [1, 5]
+p mutual_factors(8, 4)              # [1, 2, 4]
+p mutual_factors(8, 4, 10)          # [1, 2]
+p mutual_factors(12, 24)            # [1, 2, 3, 4, 6, 12]
+p mutual_factors(12, 24, 64)        # [1, 2, 4]
+p mutual_factors(22, 44)            # [1, 2, 11, 22]
+p mutual_factors(22, 44, 11)        # [1, 11]
+p mutual_factors(7)                 # [1, 7]
+p mutual_factors(7, 9)              # [1]
+```
+
+### tribonacci_number
+
+The tribonacci sequence is similar to that of [Fibonacci](https://en.wikipedia.org/wiki/Fibonacci_number). The first three numbers of the tribonacci sequence are 1, 1, and 2. To generate the next number of the sequence, we take the sum of the previous three numbers. The first six numbers of tribonacci sequence are:
+
+```ruby
+1, 1, 2, 4, 7, 13, ... and so on
+```
+Write a method `tribonacci_number` that accepts a number argument, n, and returns the n-th number of the tribonacci sequence.
+
+Examples
+
+```ruby
+p tribonacci_number(1)  # 1
+p tribonacci_number(2)  # 1
+p tribonacci_number(3)  # 2
+p tribonacci_number(4)  # 4
+p tribonacci_number(5)  # 7
+p tribonacci_number(6)  # 13
+p tribonacci_number(7)  # 24
+p tribonacci_number(11) # 274
+```
+
+## Phase 3: Now we're having fun.
+
+### matrix_addition_reloaded
+
+Write a method `matrix_addition_reloaded` that accepts any number of matrices as arguments. The method should return a new matrix representing the sum of the arguments. Matrix addition can only be performed on matrices of similar dimensions, so if all of the given matrices do not have the same "height" and "width", then return `nil`.
+
+Examples
+
+```ruby
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+matrix_f = [[0 , 0], [12, 4, 4], [6,  3]]
+
+p matrix_addition_reloaded(matrix_a, matrix_b)              # [[11, 6], [7, 7]]
+p matrix_addition_reloaded(matrix_a, matrix_b, matrix_c)    # [[10, 6], [7, 6]]
+p matrix_addition_reloaded(matrix_e)                        # [[0, 0], [12, 4], [6, 3]]
+p matrix_addition_reloaded(matrix_d, matrix_e)              # [[2, -5], [19, 14], [6, 4]]
+p matrix_addition_reloaded(matrix_a, matrix_b, matrix_e)    # nil
+p matrix_addition_reloaded(matrix_d, matrix_e, matrix_c)    # nil
+p matrix_addition_reloaded(matrix_d, matrix_e, matrix_f)    # nil
+```
