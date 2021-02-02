e772fdfd4ff2e8e4d740fea9c904d5c998107c77
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-02 20:39:25 +0100

    done phase 3

--- exercises/perilous_procs/perilous_procs.rb
+++ exercises/perilous_procs/perilous_procs.rb
@@ -208,6 +208,241 @@ p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
 # ["boot", "bug", "boat", "cat", "dog"]
 puts "--------------"
 
+def selected_map!(arr, prc_1, prc_2)
+  arr.each_with_index { |ele, idx| arr[idx] = prc_2.call(ele) if prc_1.call(ele) }
+  nil
+end
+
+is_even = Proc.new { |n| n.even? }
+is_positive = Proc.new { |n| n > 0 }
+square = Proc.new { |n| n * n }
+flip_sign = Proc.new { |n| -n }
+
+arr_1 = [8, 5, 10, 4]
+p selected_map!(arr_1, is_even, square)     # nil
+p arr_1                                     # [64, 5, 100, 16]
+
+arr_2 = [-10, 4, 7, 6, -2, -9]
+p selected_map!(arr_2, is_even, flip_sign)  # nil
+p arr_2                                     # [10, -4, 7, -6, 2, -9]
+
+arr_3 = [-10, 4, 7, 6, -2, -9]
+p selected_map!(arr_3, is_positive, square) # nil
+p arr_3                                     # [-10, 16, 49, 36, -2, -9]
+puts "--------------"
+
+def chain_map(val, arr_prcs)
+  idx = 0
+  while idx < arr_prcs.length
+    val = arr_prcs[idx].call(val)
+    idx += 1
+  end
+  val
+end
+
+add_5 = Proc.new { |n| n + 5 }
+half = Proc.new { |n| n / 2.0 }
+square = Proc.new { |n| n * n }
+
+p chain_map(25, [add_5, half])          # 15.0
+p chain_map(25, [half, add_5])          # 17.5
+p chain_map(25, [add_5, half, square])  # 225
+p chain_map(4, [square, half])          # 8
+p chain_map(4, [half, square])          # 4
+puts "--------------"
+
+def proc_suffix(stc, hash)
+  words = stc.split(" ")
+  suf_words = []
+  new_word = ""
+  
+  words.each do |word|
+    new_word = word
+
+    hash.each do |key, val|
+      if key.call(word)
+        new_word += val
+      end
+    end
+
+    suf_words << new_word
+  end
+
+  suf_words.join(" ")
+end
+
+contains_a = Proc.new { |w| w.include?('a') }
+three_letters = Proc.new { |w| w.length == 3 }
+four_letters = Proc.new { |w| w.length == 4 }
+
+p proc_suffix('dog cat',
+    contains_a => 'ly',
+    three_letters => 'o'
+)   # "dogo catlyo"
+
+p proc_suffix('dog cat',
+    three_letters => 'o',
+    contains_a => 'ly'
+)   # "dogo catoly"
+
+p proc_suffix('wrong glad cat',
+    contains_a => 'ly',
+    three_letters => 'o',
+    four_letters => 'ing'
+)   # "wrong gladlying catlyo"
+
+p proc_suffix('food glad rant dog cat',
+    four_letters => 'ing',
+    contains_a => 'ly',
+    three_letters => 'o'
+)   # "fooding gladingly rantingly dogo catlyo"
+
+puts "--------------"
+
+def proctition_platinum(arr, *prcs)
+  hash = Hash.new { |h,k| h[k] = [] }
+
+  prcs.each_with_index do |prc, idx|
+    arr.reverse.reverse_each do |ele| # Had a problem deleting element and the each jumped one element, so a reversed and counted from the end to be able to remove the element and keep the order.
+      if prc.call(ele)
+        hash[idx+1] << ele
+        arr.delete(ele)
+      end
+    end
+  end
+
+
+  hash
+end
+
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+begins_w = Proc.new { |s| s.downcase[0] == 'w' }
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, is_upcase, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["WHO", "WHY"], 3=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "WHERE!", "WHY"], 2=>["when!"], 3=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], begins_w, is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "what", "when!", "WHERE!", "WHY"], 2=>[], 3=>[], 4=>[]} 
+# I don't know why only the last one show the empty arrays, decided to omit them for all the tests.
+
+puts "--------------"
+
+def procipher(stc, hash)
+  words = stc.split(" ")
+  new_words = []
+
+  words.each do |word|
+    new_word = word
+    hash.each do |key, val|
+      if key.call(word)
+        new_word = val.call(new_word)
+      end
+    end
+    new_words << new_word
+  end
+
+  new_words.join(" ")
+end
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+make_question = Proc.new { |s| s + '???' }
+reverse = Proc.new { |s| s.reverse }
+add_smile = Proc.new { |s| s + ':)' }
+
+p procipher('he said what!',
+    is_yelled => make_question,
+    contains_a => reverse
+) # "he dias ???!tahw"
+
+p procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => make_question
+) # "he dias !tahw???"
+
+p procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => add_smile
+) # "he dias !tahw:)"
+
+p procipher('stop that taxi now',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "stop that??? taxi??? now"
+
+p procipher('STOP that taxi now!',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "STOP:) that??? taxi??? !won"
+
+puts "--------------"
+
+def picky_procipher(stc, hash)
+  words = stc.split(" ")
+  new_words = []
+
+  words.each do |word|
+    new_word = word
+    changed = false
+
+    hash.each do |key, val|
+      if key.call(word) && !changed
+        new_word = val.call(new_word)
+        changed = true                # better solution than the reverse used on the proctition_platinum?
+      end
+    end
+
+    new_words << new_word
+  end
+
+  new_words.join(" ")
+end
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+make_question = Proc.new { |s| s + '???' }
+reverse = Proc.new { |s| s.reverse }
+add_smile = Proc.new { |s| s + ':)' }
+
+p picky_procipher('he said what!',
+    is_yelled => make_question,
+    contains_a => reverse
+) # "he dias what!???"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => make_question
+) # "he dias !tahw"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => add_smile
+) # "he dias !tahw"
+
+p picky_procipher('stop that taxi now',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "stop that??? taxi??? now"
+
+p picky_procipher('STOP that taxi!',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "STOP:) that??? !ixat"
 
 puts "--------------"
 puts "--------------"
