b42c9b68f9a7e4506b8fed8a7c6c170f6b1a8c0e
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-07 18:57:20 +0100

    done exercise

--- exercises/enumerables/enumerables.rb
+++ exercises/enumerables/enumerables.rb
@@ -41,18 +41,70 @@ class Array
   end
 
   def my_flatten
+    return [self] if !self.is_a?(Array)
+
     flat_arr = []
 
     self.my_each do |ele|
-      debugger
-      if !ele.is_a?(Array)
-        flat_arr += [ele] 
+      # debugger
+      if ele.is_a?(Array)
+        flat_arr += ele.my_flatten
       else
-        ele.my_each { |arr| arr.my_flatten }
+        flat_arr += [ele] 
       end
 
     end
-
     flat_arr
+
+  end
+
+  def my_zip(*args)
+    new_arr = []
+
+    (0...self.length).each do |idx_1|
+      new_arr << [self[idx_1]]
+      (0...args.length).each do |idx_2|
+        new_arr[idx_1] << args[idx_2][idx_1]
+      end
+    end
+
+    new_arr
+  end
+
+  def my_rotate(n = 1)
+    new_arr = self
+
+    if n > 0
+      n.times do 
+        first_element = new_arr.shift
+        new_arr.push(first_element)
+      end
+    else
+      n = -n
+      n.times do 
+        last_element = new_arr.pop
+        new_arr.unshift(last_element)
+      end
+    end
+
+    new_arr
+  end
+
+  def my_join(separator = "")
+    new_str = ""
+    
+    self.each { |char| new_str += char + separator}
+
+    new_str.slice!(-1) if separator != "" 
+
+    new_str
+  end
+
+  def my_reverse
+    new_arr = []
+
+    self.reverse_each { |ele| new_arr << ele }
+
+    new_arr
   end
 end
