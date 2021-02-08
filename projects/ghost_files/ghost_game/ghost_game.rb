1768e5b4d6372868a7bca69464f901931d22f4ec
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-08 11:42:33 +0100

    basic structure of the host game

---
+++ projects/ghost_files/ghost_game/ghost_game.rb
@@ -0,0 +1,74 @@
+require_relative './player.rb'
+
+class Game
+  attr_reader :players, :dictionary
+  MAX_LOSES = 5
+  ALPHABET = Set.new("a".."z")
+
+  def initialize(*players)
+    @players = []
+    players.each { |player| @players << Player.new(player) }
+    words = File.readlines('../dictionary.txt').map(&:chomp)
+    @dictionary = Set.new(words)
+
+    @active_players = @players.map { |ele| ele }
+    @current_player = @active_players[0]
+    @losses = Hash.new(0)
+  end
+  
+  def play_round
+    take_turn(@current_player)
+    next_player!
+  end
+
+  def take_turn(player)
+    puts "Make your guess #{player.name}, current fragment is '#{@fragment}"
+    guess = gets.chomp
+
+    if self.valid_play?(guess)
+      @fragment += guess
+    else
+      puts "the #{guess} was invalid, the fragment is #{@fragment}"
+      take_turn(@current_player)
+    end
+
+    if @dictionary.include?(@fragment)
+      @losses[player] += 1
+      @fragment = ""
+      puts "#{@current_player} guessed a real word and loses. His number of losses is: #{@loses[player]}"
+    end
+  end
+
+  def valid_play?(letter)
+    return false if letter.length > 1
+    return false if !ALPHABET.include?(letter)
+
+    guess = @fragment + letter
+    @dictionary.any? { |word| word.start_with?(guess) }
+  end
+
+  def next_player!
+    current_player_idx = @active_players.index(@current_player)
+    next_player_idx = (current_player_idx + 1) % @active_players.length
+    @current_player = @active_players[next_player_idx]
+  end
+
+  def run
+    @fragment = ""
+
+    while @active_players.length != 1 do
+      self.play_round
+      @active_players.each do |player|
+        if @losses[player] == MAX_LOSES
+          puts "#{player.name} has been eliminated from the game with #{MAX_LOSES} losses." 
+          @active_players.delete(player)
+        end
+      end
+    end
+
+    puts "#{@current_player.name} wins!"
+  end
+end
+
+my_game = Game.new("p1", "p2", "p3")
+my_game.run
---
+++ projects/ghost_files/ghost_game/player.rb
@@ -0,0 +1,13 @@
+class Player
+  def initialize(name)
+    @name = name
+  end
+
+  def guess(letter)
+
+  end
+
+  def alert_invalid_guess
+
+  end
+end
