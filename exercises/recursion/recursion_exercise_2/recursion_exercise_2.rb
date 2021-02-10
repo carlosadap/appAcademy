046322dcaaabed2470e88c6b02325971696ed1dc
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-10 20:09:53 +0100

    finish exercises

--- exercises/recursion/recursion_exercise_2/recursion_exercise_2.rb
+++ exercises/recursion/recursion_exercise_2/recursion_exercise_2.rb
@@ -23,4 +23,52 @@ p add_numbers([1,2,3,4]) # => returns 10
 p add_numbers([3]) # => returns 3
 p add_numbers([-80,34,7]) # => returns -39
 p add_numbers([]) # => returns nil
+puts "----------------"
 
+def gamma_fnc(num)
+  return nil if num == 0
+  return 1 if num == 1
+
+  (num-1)*gamma_fnc(num-1)
+end
+
+# Test Cases
+p  gamma_fnc(0)  # => returns nil
+p  gamma_fnc(1)  # => returns 1
+p  gamma_fnc(4)  # => returns 6
+p  gamma_fnc(8)  # => returns 5040
+puts "----------------"
+
+def ice_cream_shop(flavors, favorite)
+  return true if flavors.pop == favorite
+  return false if flavors.empty?
+
+  ice_cream_shop(flavors, favorite)
+end
+
+
+p  ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  
+# => returns false
+p  ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  
+# => returns true
+p  ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  
+# => returns false
+p  ice_cream_shop(['moose tracks'], 'moose tracks')  
+# => returns true
+p  ice_cream_shop([], 'honey lavender')  
+# => returns false
+puts "----------------"
+
+def reverse(string)
+  return string if string.length <= 1
+
+  reverse(string[1..-1]) + string[0]
+end
+
+# Test Cases
+p reverse("house") # => "esuoh"
+p reverse("dog") # => "god"
+p reverse("atom") # => "mota"
+p reverse("q") # => "q"
+p reverse("id") # => "di"
+p reverse("") # => ""
